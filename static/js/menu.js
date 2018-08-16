0cf75a4e01259a0766b4f6c4af13eb02f15b82f6
laurent <gillet_l@hotmail.com>
2018-08-16 23:29:38 +0200

    feat contact page Layout of the contact form

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,15 +1,15 @@
-$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
+$(function() { var logo = $(".lrg-Jimmy"); $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 200) {
-        if(!logo.hasClass("sml-logo")) {
-            // logo.hide();
-            logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
+        if(!logo.hasClass("sml-Jimmy")) {
+            // Jimmy.hide();
+            logo.removeClass('lrg-Jimmy').addClass("sml-Jimmy").fadeIn( "slow");
         }
     } else {
-        if(!logo.hasClass("lrg-logo")) {
-            // logo.hide();
-            logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
+        if(!logo.hasClass("lrg-Jimmy")) {
+            // Jimmy.hide();
+            logo.removeClass("sml-Jimmy").addClass('lrg-Jimmy').fadeIn( "slow");
         }
     }
 
