48051cda41a4a2f69a3e3ac4c0c3ce025fc4ef2b
e-maxime <enrietto.maxime@gmail.com>
2018-08-21 13:06:23 +0200

    Modify url path for images

--- static/js/social-icon.js
+++ static/js/social-icon.js
@@ -1,90 +1,90 @@
 $(function(){
     $("#icon-fb").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/facebook_purple.png');
+            $(this).attr('src','/img/icones/facebook_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/facebook_black.png');
+            $(this).attr('src','/img/icones/facebook_black.png');
         }
     });
     $("#icon-twitter").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/twitter_purple.png');
+            $(this).attr('src','/img/icones/twitter_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/twitter_black.png');
+            $(this).attr('src','/img/icones/twitter_black.png');
         }
     });
     $("#icon-linkedin").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/linkedin_purple.png');
+            $(this).attr('src','/img/icones/linkedin_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/linkedin_black.png');
+            $(this).attr('src','/img/icones/linkedin_black.png');
         }
     });
     $("#icon-instagram").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/instagram_purple.png');
+            $(this).attr('src','/img/icones/instagram_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/instagram_black.png');
+            $(this).attr('src','/img/icones/instagram_black.png');
         }
     });
     $("#icon-contact").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/contact_purple.png');
+            $(this).attr('src','/img/icones/contact_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/contact_black.png');
+            $(this).attr('src','/img/icones/contact_black.png');
         }
     });
     $("#icon-news").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/news_purple.png');
+            $(this).attr('src','/img/icones/news_purple.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/news_black.png');
+            $(this).attr('src','/img/icones/news_black.png');
         }
     });
     $("#icon-fb-footer").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/facebook_blue.png');
+            $(this).attr('src','/img/icones/facebook_blue.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/facebook_black.png');
+            $(this).attr('src','/img/icones/facebook_black.png');
         }
     });
     $("#icon-twitter-footer").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/twitter_blue.png');
+            $(this).attr('src','/img/icones/twitter_blue.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/twitter_black.png');
+            $(this).attr('src','/img/icones/twitter_black.png');
         }
     });
     $("#icon-linkedin-footer").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/linkedin_blue.png');
+            $(this).attr('src','/img/icones/linkedin_blue.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/linkedin_black.png');
+            $(this).attr('src','/img/icones/linkedin_black.png');
         }
     });
     $("#icon-instagram-footer").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/instagram_blue.png');
+            $(this).attr('src','/img/icones/instagram_blue.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/instagram_black.png');
+            $(this).attr('src','/img/icones/instagram_black.png');
         }
     });
     $("#icon-contact-footer").on({
         mouseenter: function(){
-            $(this).attr('src','img/icones/contact_blue.png');
+            $(this).attr('src','/img/icones/contact_blue.png');
         },
         mouseleave: function(){
-            $(this).attr('src','img/icones/contact_black.png');
+            $(this).attr('src','/img/icones/contact_black.png');
         }
     });
 
