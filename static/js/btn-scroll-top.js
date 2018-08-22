ba04950568ac5828ae3d79b9325b8da8eb0907a3
e-maxime <enrietto.maxime@gmail.com>
2018-08-22 17:18:57 +0200

    Change URL for section services. Change code for scroll top button, add smooth scroll

--- static/js/btn-scroll-top.js
+++ static/js/btn-scroll-top.js
@@ -1,16 +1,8 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        $('.scrolltop').stop(true, true).fadeOut();
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".section-slide").offset().top},"1000");return false})})
